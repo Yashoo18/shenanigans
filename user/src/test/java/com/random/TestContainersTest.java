eabda35775b4ff5d916018537085f9010228056c
yashoo <yashoo5571@gmail.com>
2023-09-29 18:40:04 -0400

    added test containers and mockito

---
+++ user/src/test/java/com/random/TestContainersTest.java
@@ -0,0 +1,84 @@
+package com.random;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+
+import java.util.Objects;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestContainersTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        mySQLContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mySQLContainer.stop();
+    }
+
+    @Test
+    public void getAllTheCoursesTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/getall")
+                        .accept("application/json")
+                        .contentType("application/json"))
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    public void createUserTest() throws Exception {
+        User user = new User(null,"daniel","daniel@gmail.com");
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/create")
+                        .contentType("application/json")
+                        .content(asJsonString(user))
+                        .accept("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+    }
+
+    private String asJsonString(Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
